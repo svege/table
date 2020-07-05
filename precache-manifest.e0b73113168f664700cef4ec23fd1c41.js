21e7ea20c77303339bf11fcc87841bde57534501
Zveta <svet.v.gorbunova@gmail.com>
2020-07-06 01:16:46 +0300

    Updates

---
+++ precache-manifest.e0b73113168f664700cef4ec23fd1c41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a67562af58fb39f6d455afab1522139",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "770b9fadb210ed8f4196",
+    "url": "/table/static/css/main.4938b4c2.chunk.css"
+  },
+  {
+    "revision": "767f1be8bd4d51df6cfe",
+    "url": "/table/static/js/2.e4023899.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/table/static/js/2.e4023899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770b9fadb210ed8f4196",
+    "url": "/table/static/js/main.056b8022.chunk.js"
+  },
+  {
+    "revision": "c4e399e0518a7180229c",
+    "url": "/table/static/js/runtime-main.ab4727b9.js"
+  },
+  {
+    "revision": "5ef05461d1158f05fa25662bbd968005",
+    "url": "/table/static/media/OpenSans.5ef05461.woff2"
+  },
+  {
+    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
+    "url": "/table/static/media/OpenSans.90327f5a.woff"
+  }
+]);
